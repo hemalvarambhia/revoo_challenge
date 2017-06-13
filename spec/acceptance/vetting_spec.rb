acc3fed5d0b114a6ac5b85952fcd80bac5ab7f29
Hemal Varambhia <hvarambhia@bookingbug.com>
2017-06-13 09:12:58 +0100

    Made the duplication in the violation method more pronouced, removed redundant code.

--- spec/acceptance/vetting_spec.rb
+++ spec/acceptance/vetting_spec.rb
@@ -24,7 +24,7 @@ describe "I want reviews to be vetted" do
 
     class PricesRule
       def violation(review)
-        if price_contained_in?(review)
+        if contained_in?(review)
           return "Sorry you can't mention the price"
         end
 
@@ -32,15 +32,15 @@ describe "I want reviews to be vetted" do
       end
 
       private
-      
-      def price_contained_in?(review)
+
+      def contained_in?(review)
         review.text.scan(/£\d+/).any?
       end
     end
 
     class RepetitionRule
       def violation(review)
-        if repetition_contained_in?(review)
+        if contained_in?(review)
           return "Sorry you can't have repetition"
         end
 
@@ -49,7 +49,7 @@ describe "I want reviews to be vetted" do
 
       private
 
-      def repetition_contained_in? review
+      def contained_in? review
         word_count(review).any? { |_, count| count == 3 }
       end
 
@@ -68,38 +68,19 @@ describe "I want reviews to be vetted" do
         @offensive_words = offensive_words
       end
       
-      def bad_language_contained_in?(review)
-        offensive_words.any? do |offensive_word|
-          review.contains? offensive_word
-        end
-      end
-
       def violation(review)
-        if bad_language_contained_in?(review)
+        if contained_in?(review)
           return "Sorry you can't use bad language"
         end
 
         nil
       end
-    end
-    
-    def self.bad_language_contained_in?(review)
-      offensive_words = %w{hamster PHP Brainfuck elderberry}
-      offensive_words.any? do |offensive_word|
-        review.contains? offensive_word
-      end
-    end
-    
-    def self.reject_for_bad_language(review)
-      review.reject_for("Sorry you can't use bad language")
-    end
 
-    def self.bad_language_violation(review)
-      if bad_language_contained_in?(review)
-        return "Sorry you can't use bad language"
+      def contained_in?(review)
+        offensive_words.any? do |offensive_word|
+          review.contains? offensive_word
+        end
       end
-
-      nil
     end
   end
   

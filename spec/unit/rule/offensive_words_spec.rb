426c4fc1948ab20ac3bfd74bada35ad3b74ae608
Hemal N Varambhia <h.n.varambhia@gmail.com>
2017-06-15 22:27:55 +0100

    A review without any offensive words does not violate the rule.

--- spec/unit/rule/offensive_words_spec.rb
+++ spec/unit/rule/offensive_words_spec.rb
@@ -1,9 +1,23 @@
 require 'spec_helper'
+require 'review'
 require 'rule/offensive_words'
 
 describe OffensiveWords do
+  let(:list) { %w{Hate Politics Humanities} }
+  subject(:offensive_words) do
+    OffensiveWords.new(list)
+  end
+  
   describe 'a review with no offensive words' do
-    it 'is not a violation'
+    let(:without_offensive_words) do
+      Review.new('id' => 1, 'text' => 'Great product, really useful.')
+    end
+      
+    it 'is not a violation' do
+      violation = offensive_words.violation(without_offensive_words)
+      
+      expect(violation).to be_nil
+    end
   end
   
   describe 'a review with one offensive word' do

ef4d9fe4de657356c3ac9e60c45bb5d57d0402c7
Hemal Varambhia <hvarambhia@bookingbug.com>
2017-06-15 14:30:09 +0100

    Test list for offensive words.

---
+++ spec/unit/rule/offensive_words_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'rule/offensive_words'
+
+describe OffensiveWords do
+  it "rejects reviews with the word 'Elderberry'"
+
+  it "rejects reviews with the word 'elderberry"
+
+  it "rejects reviews with the word 'PHP'"
+
+  it "rejects reviews with the word 'php'"
+
+  it "rejects reviews with the word 'Brainfuck'"
+
+  it "rejects reviews with the word 'brainfuck'"
+end

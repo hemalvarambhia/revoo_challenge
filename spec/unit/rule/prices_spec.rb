720378a959d86c11a4858e0f1ef89aaafef51e29
Hemal Varambhia <hvarambhia@bookingbug.com>
2017-06-19 13:13:18 +0100

    Refactor - moved the reviews to intent-revealing helper methods, eliminated duplication in setting up the rule.

--- spec/unit/rule/prices_spec.rb
+++ spec/unit/rule/prices_spec.rb
@@ -4,27 +4,42 @@ require 'review'
 require 'rule/prices'
 
 describe Prices do
+  let(:prices) { Prices.new }
+  let(:expected_message) { "Sorry you can't mention the price" }
+
   describe 'a review with no prices quoted' do
+    let(:no_prices) do
+      Review.new('text' => 'No prices here!')
+    end
+
     it 'is not a violation' do
-      violation = Prices.new.violation(Review.new('text' => 'No prices here!'))
+      violation = prices.violation(no_prices)
 
       expect(violation).to be_nil
     end
   end
 
   describe 'a review with a price quoted' do
-    it 'is a violation' do      
-      violation = Prices.new.violation(Review.new('text' => 'It cost £15 - too expensive!'))
+    let(:one_price_quoted) do
+      Review.new('text' => 'It cost £15 - too expensive!')
+    end
 
-      expect(violation).to eq "Sorry you can't mention the price"
+    it 'is a violation' do      
+      violation = prices.violation(one_price_quoted)
+
+      expect(violation).to eq expected_message
     end
   end
 
   describe 'a review with more than one price quoted' do
-    it 'is a violation' do
-      violation = Prices.new.violation(Review.new('text' => '15! It was only worth £10!'))
+    let(:multiple_prices_quoted) do
+      Review.new('text' => "It cost £15 - too expensive! It's only worth £10")
+    end
 
-      expect(violation).to eq "Sorry you can't mention the price"
+    it 'is a violation' do
+      violation = prices.violation(multiple_prices_quoted)
+
+      expect(violation).to eq expected_message
     end
   end
 
